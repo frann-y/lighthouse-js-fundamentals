5d333dd48697afd30cc737139afa94df9cdbcf65
frann-y <fgranger00@gmail.com>
2018-11-20 13:49:31 +0000

    Concatenate Arrays assignment

---
+++ concatenate-arrays.js
@@ -0,0 +1,8 @@
+// Define a function concat, which,
+//given two arrays, concatenates the arrays together.
+
+function concat (arr1, arr2) {
+  return arr1.concat(arr2);
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]));
